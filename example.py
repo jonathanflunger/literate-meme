d8c9a986a0bb99f61208b3e4fd1ad792c7023cf8
Jonathan Flunger <jonathan.flunger@ucdconnect.ie>
2023-10-25 09:59:21 +0200

    updated example.py

--- example.py
+++ example.py
@@ -1,5 +1,21 @@
-def print_letter(letter):
-    print(letter)
+def calc_dry_gas_constant():
+    m_n2 = 28.0134
+    m_o2 = 31.9988
+    m_ar = 39.948
+    m_co2 = 44.0095
+
+    n_n2 = 0.78084
+    n_o2 = 0.20946
+    n_ar = 0.00934
+    n_co2 = 0.000314
+
+    m_avg = (m_n2 * n_n2) + (m_o2 * n_o2) + (m_ar * n_ar) + (m_co2 * n_co2)
+
+    print(m_avg)
+
+    R = 8.31446261815324
+    R_dry = R / (m_avg/1000)
+    return R_dry
 
 if __name__ == '__main__':
-    print_letter('a')
+    print(calc_dry_gas_constant())
